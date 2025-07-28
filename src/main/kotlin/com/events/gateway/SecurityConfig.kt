5e5ee4e11ce06d71f46b82bed8f8bbbb060bd555
leandrodalbo <leandro.edbo@gmail.com>
2025-07-28 16:53:11 -0300

    fixing cors issue

--- src/main/kotlin/com/events/gateway/SecurityConfig.kt
+++ src/main/kotlin/com/events/gateway/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.events.gateway
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -13,10 +14,11 @@ class SecurityConfig {
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
-            .cors { it.disable() }
+
             .csrf { it.disable() }
             .authorizeExchange {
                 it
+                    .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .pathMatchers("/api/public/**", "/auth/**").permitAll()
                     .anyExchange().authenticated()
             }
